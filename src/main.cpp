25c6499ffa7578a98821a3b7c336f0f6b8abc0b7
Kian Dadkhah <contact.kiands@gmail.com>
2025-01-18 16:20:07 -0500

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+// LINUX FILE MANAGER
+// -------------------
+// author:  @techapostle (Kian Dadkhah)
+// date:    1/18/2025
+// license: GPL-3.0
+// version: 0.1.0 (alpha)
+// -------------------
+// description:
+// This is a simple file manager for Linux systems. It is written in C++ and uses the ncurses library for the user interface.
+// -------------------
+// usage:
+// $ ./lfm
+// -------------------
+// dependencies:
+// - ncurses
+// -------------------
+
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  std::cout << "Welcome to the LFM" << std::endl;
+
+  return 0;
+}
